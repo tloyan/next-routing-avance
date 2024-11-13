5a0033e3bd61c63b4c021f6781e6f6493c11f38c
tloyan <thomas.loyan@gmail.com>
2024-11-13 12:29:12 +0100

    feature/intercept-routes

---
+++ src/app/(backoffice)/intercept/(..)cgv/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>CGV intercepted, valide ici les CGV</h1>
+      <p>
+        {`Utilisateur du backOffice, En utilisant ce service, vous acceptez les précautions d'utilisation.
+        Veuillez les lire attentivement avant d'accéder ou d'utiliser le
+        service.`}
+      </p>
+    </div>
+  )
+}
